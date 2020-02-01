28749695a3eae35bc42e7671153e78a1e9e75378
Ivari Horm <ivari@baltnet.ee>
2020-02-01 22:13:00 +0200

    Windows cannot properly detect node_modules directory for NPX command.

--- project/Webpack.scala
+++ project/Webpack.scala
@@ -1,4 +1,3 @@
-import play.sbt.PlayRunHook
 import sbt.io.syntax.File
 
 import scala.sys.process.Process
@@ -12,13 +11,13 @@ object Webpack {
 
 			override def beforeStarted(): Unit = {
 				process = Option(
-					Process("npx webpack --display=errors-only", base).run()
+					Process(npx + "webpack --display=errors-only", base).run()
 				)
 			}
 
 			override def afterStarted(): Unit = {
 				process = Option(
-					Process("npx webpack --watch --display=errors-only", base).run()
+					Process(npx + "webpack --watch --display=errors-only", base).run()
 				)
 			}
 
@@ -33,5 +32,14 @@ object Webpack {
 
 	def dist(base: File): Unit = {
 		if (Process("npx webpack", base).! != 0) throw new Exception("Something goes wrong when running webpack.")
+	}
+
+	def npx(): String = {
+		System.getProperty("os.name").toLowerCase match {
+			// Windows npx.cmd is buggy detecting node_modules folder inside NodeJS when invoked via SBT Process() function.
+			// Use npx.cmd from .bin folder inside build directory, cross the fingers and hope for the best.
+			case win if win.contains("win") => "node_modules/.bin/npx.cmd "
+			case _ => "npx "
+		}
 	}
 }
